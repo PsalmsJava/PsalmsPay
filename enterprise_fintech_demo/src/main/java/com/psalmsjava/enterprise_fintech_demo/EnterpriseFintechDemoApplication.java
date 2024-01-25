6518546c950c5d25bb20dd0dc06beff52edf5fd1
PsalmsJava <inyangpsalms@gmail.com>
2024-01-25 08:35:09 +0100

    Bootstrapped Project

---
+++ enterprise_fintech_demo/src/main/java/com/psalmsjava/enterprise_fintech_demo/EnterpriseFintechDemoApplication.java
@@ -0,0 +1,13 @@
+package com.psalmsjava.enterprise_fintech_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseFintechDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseFintechDemoApplication.class, args);
+	}
+
+}
---
+++ enterprise_fintech_demo/src/test/java/com/psalmsjava/enterprise_fintech_demo/EnterpriseFintechDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psalmsjava.enterprise_fintech_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseFintechDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
